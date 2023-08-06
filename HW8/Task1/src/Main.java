557ca0b66e09afa0527cb7c3f72f6972c7405ec9
dem0N4k <demON4k1993@gmail.com>
2023-08-07 01:19:29 +0300

    finish

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,44 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+
+        /*int[] array = new int[6];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt(100);
+        }*/
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+        System.out.println("\nСледующее задание\n");
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
 
+        for (int i = (array.length - 1); i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("\nСледующее задание\n");
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+
+
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
+
+
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        double middleWeight = 0;
+        int peopleCoumt = 0;
+
+        double[] weights = new double[10];
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random().nextDouble() * 60) + 40;
+            middleWeight += weights[i];
+            if (weights[i] >= 60 && weights[i] <= 80) {
+                peopleCoumt++;
+            }
+        }
+        System.out.println("Средний вес:" + middleWeight / weights.length);
+        System.out.println("Кол-во людей в весе от 60 до 80 кг: " + peopleCoumt);
     }
 }
+
